b96acd525fc43ede2b53b69d0d1a40572038430b
myxyy <m.myxomatosis@gmail.com>
2023-09-24 21:25:45 +0900

    一応学習と推論ができる

--- sconv/src/retnet_conv.py
+++ sconv/src/retnet_conv.py
@@ -23,6 +23,7 @@ class RetentionConv(nn.Module):
         self.dim_qkv = dim_qkv
         self.num_head = num_head
         self.phazor = nn.Parameter(torch.randn((num_head, dim_qkv), dtype=torch.cfloat)) # log(-log(gamma))
+        self.amplitude = nn.Parameter(torch.randn(num_head))
         #self.phazor_init = nn.Parameter(torch.randn((num_head, dim_qkv, dim_qkv), dtype=torch.cfloat)) # log(-log(gamma))
         self.last_conv = None # (batch, num_head, dim_qkv, dim_qkv)
         self.last_conv_init = nn.Parameter(torch.randn((num_head, dim_qkv, dim_qkv), dtype=torch.cfloat)) # (num_head, dim_qkv, dim_qkv)
@@ -31,6 +32,7 @@ class RetentionConv(nn.Module):
         self.wv = nn.Linear(dim, num_head * dim_qkv)
         self.wout = nn.Linear(num_head * dim_qkv, dim)
         self.is_refresh = True
+        self.sigmoid = nn.Sigmoid()
 
     # (batch, len, dim) -> (batch, len, dim)
     def forward(self, x):
@@ -49,20 +51,21 @@ class RetentionConv(nn.Module):
             self.last_conv = self.last_conv_init.unsqueeze(0).expand(batch, self.num_head, self.dim_qkv, self.dim_qkv) 
 
         phase = self.phazor / self.phazor.abs()
-        amplitude = torch.exp(-self.phazor.abs()).mean(1) # (num_head,)
+        amplitude = self.sigmoid(self.amplitude) # (num_head,)
         phazor = phase * amplitude.unsqueeze(-1).expand(num_head, dim_qkv) # (num_head, dim_qkv)
         phase_progression = torch.pow(phase.unsqueeze(0), (torch.arange(len, device=x.device)).unsqueeze(1).unsqueeze(1)).expand(len, num_head, dim_qkv) # (len, num_head, dim_qkv)
         phase_progression_inverse = torch.pow(phase.unsqueeze(0), (-torch.arange(len, device=x.device)).unsqueeze(1).unsqueeze(1)).expand(len, num_head, dim_qkv) # (len, num_head, dim_qkv)
         phazor_progression = torch.pow(phazor.unsqueeze(0), (torch.arange(len, device=x.device)).unsqueeze(1).unsqueeze(1)).expand(len, num_head, dim_qkv) # (len, num_head, dim_qkv)
         phazor_progression_inverse = torch.pow(phazor.unsqueeze(0), (len - 1 - torch.arange(len, device=x.device)).unsqueeze(1).unsqueeze(1)).expand(len, num_head, dim_qkv) # (len, num_head, dim_qkv)
 
-        cross_chunk = torch.matmul(query.unsqueeze(3) * phazor_progression.unsqueeze(0).unsqueeze(3), self.last_conv.detach().unsqueeze(1)).view(batch, len, num_head, dim_qkv) # (batch, len, num_head, dim_qkv)
+        cross_chunk = torch.matmul(query.unsqueeze(3) * (phazor_progression * phazor.unsqueeze(0)).unsqueeze(0).unsqueeze(3), self.last_conv.detach().unsqueeze(1)).view(batch, len, num_head, dim_qkv) # (batch, len, num_head, dim_qkv)
         if self.is_refresh:
             self.last_conv = self.last_conv.detach() * torch.pow(phazor, len).unsqueeze(0).unsqueeze(-1) + (kv * phazor_progression_inverse.unsqueeze(0).unsqueeze(-1)).sum(1)
 
         mask_mask = torch.full((len, len), np.inf, device=x.device).triu(1) # (len, len)
+        #mask_mask_2 = torch.ones((len, len), device=x.device).tril()
         mask_exp = (torch.arange(len, device=x.device).unsqueeze(1) - torch.arange(len, device=x.device).unsqueeze(0)) + mask_mask # (len, len)
-        mask = torch.pow(amplitude.unsqueeze(-1).unsqueeze(-1), mask_exp.unsqueeze(0)) # (num_head, len, len)
+        mask = torch.pow(amplitude.unsqueeze(-1).unsqueeze(-1), mask_exp.unsqueeze(0)).detach() # (num_head, len, len) # 勾配計算するとnanになりよくわからず
         qk = torch.matmul(
             (query * phase_progression.unsqueeze(0)).permute(0,2,1,3), # (batch, num_head, len, dim_qkv)
             (key * phase_progression_inverse.unsqueeze(0)).permute(0,2,3,1) # (batch, num_head, dim_qkv, len)
@@ -71,7 +74,11 @@ class RetentionConv(nn.Module):
         inner_chunk = torch.matmul(qk_mask, value.permute(0,2,1,3).to(torch.cfloat)).permute(0,2,1,3) # (batch, len, num_head, dim_qkv)
 
         out = self.wout((inner_chunk + cross_chunk).real.reshape(batch, len, num_head * dim_qkv))
-        print(mask)
+        #print(f'test:{out.isinf().any()}')
+        #print(f'test:{out.isnan().any()}')
+        #print(amplitude)
+        #print(mask[7])
+        #print(out[0])
         return out
 
     def reset_hidden(self):
